85c9e00903815642a3ecd6f067cc73b9cc566d7a
Joel Spencer <jspencer@rokmobile.co.uk>
2015-10-16 17:14:44 +0100

    Load animations added, parent div mode functionality added

--- js/jquery.jsgallery/jquery.jsgallery.js
+++ js/jquery.jsgallery/jquery.jsgallery.js
@@ -1,26 +1,38 @@
 
 (function ( $ ) {
-	
+	var globalSelf;
 	var methods = {
 		
 		show : function(self){
-			self.css("display","block");
+			globalSelf = self;
+			setTimeout(function(){animateUI("in")}, 300);
+			//globalSelf.css("display","block");
 		},
 		
 		hide : function(self){
-			$(self).css("display","none");
+			$("*").removeClass("selected_slider_div");
+			globalSelf = self;
+			animateUI("out");
+			//$(globalSelf).css("display","none");
+			
 		}
 		
 	}
 	
 	function createGallery(self){
-		var html = '<div id="jsgall_prevButton" class="gallery-arrow arrow-left"><i class="fa fa-chevron-left"></i></div>';
-			html += '<div id="jsgall_imageContainer"><img id="jsgallery_displayed_image">';
+		var html = '<div id="jsgall_prevButton" class="gallery-arrow arrow-left start_pos"><i class="fa fa-chevron-left"></i></div>';
+			html += '<div id="jsgall_imageContainer" ><img id="jsgallery_displayed_image" class="start_size">';
 			html += '<div id="jsgall_htmloverlay"><span id="customHTMLFooter"></span></div></div>';
-			html += '<div id="jsgall_nextButton" class="gallery-arrow arrow-right"><i class="fa fa-chevron-right"></i></div>';
+			html += '<div id="jsgall_nextButton" class="gallery-arrow arrow-right start_pos"><i class="fa fa-chevron-right"></i></div>';
 			html += '<div id="jsgall_close"><i class="fa fa-times"></i></div>'; 
+			
+			
+			
 			$(self).html(html);
 		
+			
+			
+			
 	}
 	
 	function loadNextImage(){
@@ -55,12 +67,85 @@
 	}
 	
 	
+	function animateUI(dir){
+		
+		//Animate arrows
+		if(dir == "in"){
+			//globalSelf.css("background","rgba(0,0,0,0)");
+			globalSelf.css("display","block");
+			
+			//$(globalSelf).css("background","rgba(0,0,0,0.7)");
+
+			
+			setTimeout(function(){
+					$( "img#jsgallery_displayed_image" ).width("auto");
+					var imgWidth = $('img#jsgallery_displayed_image').width();
+					$( "img#jsgallery_displayed_image" ).width("0");
+					$( "img#jsgallery_displayed_image" ).animate({
+						width: imgWidth +"px",
+						opacity : 1
+					  }, 200,  function() {
+						   $( "img#jsgallery_displayed_image" ).width("auto");
+						   $('img#jsgallery_displayed_image').removeClass("start_size");
+					  });
+									
+					$('.gallery-arrow').removeClass("start_pos");
+				
+				}, 100); 
+				
+				$( "#jsgall_close" ).animate({
+						opacity : 1
+					  }, 100, function() {
+					  
+			});
+			
+		}
+		if(dir == "out"){
+			$('.gallery-arrow').addClass("start_pos");
+			$( "img#jsgallery_displayed_image" ).animate({
+						width: "0",
+						opacity : 0
+					  }, 200, function() {
+					  $('img#jsgallery_displayed_image').addClass("start_size");
+			});
+			
+			$( "#jsgall_close" ).animate({
+						opacity : 0
+					  }, 200, function() {
+					  
+			});
+					
+			setTimeout(function(){
+				$(globalSelf).css("display","none");
+				}, 300); 
+		}
+		
+		
+	}
+	
+	function createImagesArray(){
+		$.each($(config.imgSelector), function(index, element){
+		             if(element.src) {
+			          var image = {};
+			          image.src = element.src;
+			          if(element.alt && element.alt != "") image.alt = element.alt;
+			          if(element.width != 0) image.width = element.width;
+					  if(element.height != 0) image.height = element.height;
+					  if($(element).data('fb')) image.fb = $(element).data('fb');
+					  if($(element).data('tw')) image.tw = $(element).data('tw');
+					  if($(element).data('pin')) image.pin = $(element).data('pin');
+			          images.push(image);
+			          }
+	            });
+	}
+	
 	var images = [];
 	var config = {};
+	var originalSelector;
     
 	
 $.fn.jsgallery = function(optionsOrMethod) {
-	
+			
 			
 			
 			var newDiv = $('<div></div>').addClass("jsgallery-container");
@@ -85,6 +170,7 @@ $.fn.jsgallery = function(optionsOrMethod) {
 				
 			}
 			
+			originalSelector = optionsOrMethod.imgSelector;
 			
 			createGallery(self);
 			
@@ -107,13 +193,45 @@ $.fn.jsgallery = function(optionsOrMethod) {
 	    	
 	    	
 	    	$(config.imgSelector).click(function(){
+		    	config.imgSelector = originalSelector;
+		    	
+		    	var parents = $(this).parents();
+		    	
+		    	for(var i = 0; i < parents.length; i++){
+			    	if($(parents[i]).attr("media") == "slider"){
+				    	$(parents[i]).addClass("selected_slider_div");
+				    	config.imgSelector = ".selected_slider_div " + originalSelector;
+				    	
+			    	}
+		    	}
+		    	
+		    	images = [];
+	            createImagesArray();
+		    	
+		    	console.log(config.imgSelector);
+		    	
 		    	var src = this.src;
 		    	
-		    	var selectedIndex;	    	
+		    	var selectedIndex;	
+		    	
+		    	    	
+/*
 		    	var result = images.filter(function( obj , index) {
 				  if( obj.src == src) selectedIndex = index;
 				  return result;
 				});
+*/
+				
+				
+				for(var i = 0; i < images.length; i++){
+					if(images[i].src == src){
+						selectedIndex = i;
+						break;
+					}
+				}
+				
+				
+				
 				config.currentImage = selectedIndex;
 				showSelectedImage();
 				methods.show(self);
@@ -136,20 +254,7 @@ $.fn.jsgallery = function(optionsOrMethod) {
 	        return this.each( function() {
 	            
 	            images = [];
-	            $.each($(config.imgSelector), function(index, element){
-		             if(element.src) {
-			          var image = {};
-			          image.src = element.src;
-			          if(element.alt && element.alt != "") image.alt = element.alt;
-			          if(element.width != 0) image.width = element.width;
-					  if(element.height != 0) image.height = element.height;
-					  if($(element).data('fb')) image.fb = $(element).data('fb');
-					  if($(element).data('tw')) image.tw = $(element).data('tw');
-					  if($(element).data('pin')) image.pin = $(element).data('pin');
-			          images.push(image);
-			          }
-	            });
-	            
+	            createImagesArray();
 	            
 	            $('.gallery-arrow').css('display',"block");
 	            if(images.length < 2) $('.gallery-arrow').css('display',"none");
